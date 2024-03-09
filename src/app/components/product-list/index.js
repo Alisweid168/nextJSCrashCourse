fa398a8173580fe154e93b86241cb6f5acb0d6a8
Alisweid168 <ali.sweidan003@gmail.com>
2024-03-09 17:45:32 +0200

    update

--- src/app/components/product-list/index.js
+++ src/app/components/product-list/index.js
@@ -1,29 +1,39 @@
 "use client";
+import { useRouter } from "next/navigation";
 
 const products = [
   {
     id: 1,
     name: "Product 1",
-    description: "Product 1 ",
+    description: "Product 1",
   },
   {
     id: 2,
     name: "Product 2",
-    description: "Product 2 ",
+    description: "Product 2",
   },
   {
     id: 3,
     name: "Product 3",
-    description: "Product 3 ",
+    description: "Product 3",
   },
 ];
 
 export default function ProductList() {
+  const router = useRouter();
+  console.log(router);
+
   return (
     <ul>
-      {products.map((ProductItem) => (
-        <li key={ProductItem.id}>{ProductItem.name}</li>
+      {products.map((productItem) => ( // Changed ProductItem to productItem
+        <li
+          onClick={() => router.push(`/products/${productItem.id}`)}
+          key={productItem.id}
+        >
+          {productItem.name}
+        </li>
       ))}
     </ul>
   );
 }
+
